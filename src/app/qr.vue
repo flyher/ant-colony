cffae2256465bb86d0bf90e0fe970f74ad9039fa
flyher <flyher@users.noreply.github.com>
2017-08-07 16:19:38 +0800

    fix style

--- src/app/qr.vue
+++ src/app/qr.vue
@@ -2,16 +2,19 @@
   <div id="qr">
     <ui-header></ui-header>
     <div class="content">
-      <div class="col-lg-6">
-        <div class="input-group">
-          <input type="text" placeholder="url" class="form-control" v-model="url">
-          <span class="input-group-btn">
-            <button class="btn btn-success" type="button" v-on:click="createQRCode()">
-              create
-            </button>
-          </span>
+      <div class="input-url">
+        <div class="col-lg-6">
+          <div class="input-group">
+            <input type="text" placeholder="url" class="form-control" v-model="url">
+            <span class="input-group-btn">
+              <button class="btn btn-success" type="button" v-on:click="createQRCode()">
+                create
+              </button>
+            </span>
+          </div>
         </div>
       </div>
+  
       <div id="qrcode"></div>
     </div>
     <ui-footer></ui-footer>
@@ -61,6 +64,13 @@ export default {
     width: 80%;
     height: 400px;
     margin: auto;
+    .input-url {
+      width: 100%;
+      display: grid;
+      .col-lg-6 {
+        margin: auto;
+      }
+    }
     #qrcode {
       width: 130px;
       height: 130px;
